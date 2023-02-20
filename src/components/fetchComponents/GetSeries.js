e1638de709b765c00c7012b8b518e112711b34ca
fyrodems <fyrodems@gmail.com>
2023-02-20 21:42:49 +0100

    display series

--- src/components/fetchComponents/GetSeries.js
+++ src/components/fetchComponents/GetSeries.js
@@ -1,18 +1,59 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
 // import styled from "styled-components";
 // import Background from "../common/Background";
 import Container from "../common/Container";
 
 const GetSeries = () => {
+  const [series, SetSeries] = useState([]);
+
   useEffect(() => {
     (async () => {
       const response = await fetch(`https://api.tcgdex.net/v2/en/series`);
       const responseData = await response.json();
-      console.log(responseData);
+      SetSeries(responseData);
     })();
   }, []);
 
-  return <Container></Container>;
+  console.log(series);
+
+  return (
+    <Container>
+      {series.map((serie) => {
+        if (serie.logo) {
+          return (
+            <SingleSerie key={serie.id}>
+              <img alt={serie.name} src={serie.logo} />
+              <span>{serie.name}</span>
+            </SingleSerie>
+          );
+        }
+      })}
+    </Container>
+  );
 };
 
 export default GetSeries;
+
+const SingleSerie = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  gap: 30px;
+  cursor: pointer;
+
+  transition: 500ms;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+
+  img {
+    max-width: 160px;
+    max-height: 160px;
+  }
+
+  span {
+    text-align: center;
+  }
+`;
