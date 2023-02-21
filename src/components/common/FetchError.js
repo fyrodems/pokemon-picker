eb145ba10607ef23e52498182b5ead0c73589cef
fyrodems <fyrodems@gmail.com>
2023-02-21 21:25:08 +0100

    create fetch error container

---
+++ src/components/common/FetchError.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const FetchError = () => {
+  return <Info>Something went wrong. Please try again</Info>;
+};
+
+export default FetchError;
+
+const Info = styled.span``;
