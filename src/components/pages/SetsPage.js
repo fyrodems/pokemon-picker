55f2bf77e0ba5413fd98d8ef27ca504e4377ae13
fyrodems <fyrodems@gmail.com>
2023-02-23 20:25:15 +0100

    add getSets fetch to SetsPage

--- src/components/pages/SetsPage.js
+++ src/components/pages/SetsPage.js
@@ -1,5 +1,20 @@
+import styled from "styled-components";
+import Background from "../common/Background";
+import GetSets from "../fetchComponents/GetSets";
+
 const SetsPage = () => {
-  return <></>;
+  return (
+    <Container>
+      <Background />
+      <h1 className="title">Pokemon picker</h1>
+      <GetSets />
+    </Container>
+  );
 };
 
 export default SetsPage;
+
+const Container = styled.main`
+  height: calc(100vh - 70px);
+  padding: 40px;
+`;
